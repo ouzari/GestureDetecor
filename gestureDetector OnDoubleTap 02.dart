b9997c9de79ea987aaf28ff7bf4c7c47996997ce
ouzari <58311376+ouzari@users.noreply.github.com>
2020-08-30 23:43:26 +0200

    Add files via upload

---
+++ gestureDetector OnDoubleTap 02.dart
@@ -0,0 +1,76 @@
+
+import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:flutter/rendering.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,     
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+  var rnd = new Random();
+  int randomNumber = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Container(
+  alignment: FractionalOffset.center,
+  color: Colors.white,
+  child: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(randomNumber.toString()),
+      ),
+      GestureDetector(
+        onTap: () {
+          setState(() {
+            randomNumber=rnd.nextInt(100);
+          });
+        },
+        onDoubleTap: () {
+          setState(() {
+            randomNumber=0;
+          });
+        },
+        child: Container(
+          color: Colors.blue,
+          padding: const EdgeInsets.all(8),
+          child: const Text('GET RANDOM NUMBER'),
+        ),
+      ),
+    ],
+  ),
+)
+      ),
+      
+    );
+  }
+}
